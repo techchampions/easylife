c39c06981a63aac7bef6ff863df69778d4989b10
John Dev <kingsoorsa@gmail.com>
2026-03-02 13:45:02 +0100

    deploy: to netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import AppRoutes from "./routes/app.routes";
+import AppRoutes from "./routes/app_routes";
 import { QueryProvider } from "./services/QueryProvider";
 
 function App() {
